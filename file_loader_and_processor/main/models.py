5b681a763a9501cfe78f0412cad56c59dc2427e7
SGGM <gleb_somov@mail.ru>
2024-02-21 20:49:09 +0300

    file should not be null or blank, added a few route and file creation tests

--- file_loader_and_processor/main/models.py
+++ file_loader_and_processor/main/models.py
@@ -2,6 +2,6 @@ from django.db import models
 
 
 class File(models.Model):
-    file = models.FileField()
+    file = models.FileField(null=False, blank=False)
     uploaded_at = models.DateTimeField(auto_now_add=True)
     processed = models.BooleanField(default=False)
--- file_loader_and_processor/main/tests.py
+++ file_loader_and_processor/main/tests.py
@@ -1,3 +1,46 @@
+from django.urls import include, path, reverse
 from django.test import TestCase
+from rest_framework import status
+from rest_framework.test import APITestCase, URLPatternsTestCase
 
-# Create your tests here.
+from .models import File
+
+
+class FileTests(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        test_file = File.objects.create(
+            file='test_me.txt'
+        )
+
+    def test_file_processed_field_after_creation(self):
+        file = File.objects.get(id=1)
+        processed = f'{file.processed}'
+
+        self.assertEqual(processed, 'False')
+
+class FileURLTests(APITestCase, URLPatternsTestCase):
+    urlpatterns = [
+        path('api/v1/', include('main.urls')),
+    ]
+
+    @classmethod
+    def setUpTestData(cls):
+        test_file = File.objects.create(
+            file='test_me.txt'
+        )
+
+    def test_list_one_file(self):
+        url = reverse('files')
+        response = self.client.get(url, format='json')
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), 1)
+
+    def test_list_two_files(self):
+        url = reverse('files')
+        response = self.client.get(url, format='json')
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertNotEqual(len(response.data), 2)
